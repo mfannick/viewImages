9d4a6d714db183b9b831aac7fcf6fbbac21622b0
mfannick <mfannick1@gmail.com>
2019-10-13 21:04:59 +0200

    editing the views

--- images/views.py
+++ images/views.py
@@ -15,14 +15,7 @@ def homePage(request):
     images = Image.getImages()
     yes='yes'
     return render(request,'homePage.html',{"images":images,'yes':yes})
-# def homePage(request,anyDate):
-#     date = dt.datetime.strptime(anyDate, '%Y-%m-%d').date()
-#     images = Image.imageGetByDate(date)
-#     return render(request,'homePage.html',{"date": date,"images":images})
-# def homePage(request,category):
-#     category=Category.get(id)
-#     images = Image.imageGetByCategory(category=category)
-#     return render(request,'homePage.html',{"category": category,"images":images})
+})
 def searchImageByCategory(request):
     if 'image' in request.GET and request.GET['image']:
         search_term=request.GET.get('image')
@@ -34,16 +27,7 @@ def searchImageByCategory(request):
         message='no search yet'
         return render(request,'search.html',{'message':message})
 
-# def searchImageByCategory(request):
-#     if 'image' in request.GET and request.GET['image']:
-#         search_term=request.GET.get('image')
-#         images=Image.objects.filter(categoryF__categoryName__icontains=search_term)
-#         message = f"{search_term}"
 
-#         return render(request,'search.html',{'message':message,'images':images})
-#     else:
-#         message='no search yet'
-#         return render(request,'search.html',{'message':message})
 
 def imageDescription(request,imageId):
     try:
